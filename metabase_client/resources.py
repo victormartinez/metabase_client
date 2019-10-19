6aaeab9d0cad56659f5c1777096ed6ee4893380d
Victor Martinez <vcrmartinez@gmail.com>
2019-10-18 23:04:33 -0300

    Create basic resources module with Auth and Cards

---
+++ metabase_client/resources.py
@@ -0,0 +1,33 @@
+class Resource:
+
+    def __init__(self, http_client):
+        self.http_client = http_client
+
+
+class SessionResource(Resource):
+
+    PATH_LOGIN = "/api/session"
+
+    def get_token(self, username, password):
+        headers = {"Content-Type": "application/json"}
+        auth_data = {"username": username, "password": password}
+        response = self.http_client.request(
+            "post",
+            self.PATH_LOGIN,
+            headers=headers,
+            json=auth_data
+        )
+        return response.json().get("id")
+
+
+class CardResource(Resource):
+
+    PATH_LIST_CARDS = "/api/card"
+
+    def list(self, auth_token):
+        response = self.http_client.authenticated(
+            "get",
+            self.PATH_LIST_CARDS,
+            auth_token
+        )
+        return response.json()
