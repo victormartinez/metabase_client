da0e49e696aabe498445203c9153352c0a4c6057
Victor Martinez <vcrmartinez@gmail.com>
2019-12-20 16:02:46 -0300

    Test card and session resources

---
+++ tests/resources/test_session.py
@@ -0,0 +1,16 @@
+import mock
+
+from metabase_client.resources import SessionResource
+
+
+def test_get_token():
+    mock_client = mock.Mock()
+    mock_client.request.return_value = mock.Mock(json=lambda: {"id": "1q2w3e4r"})
+
+    resource = SessionResource(mock_client)
+    assert resource.get_token("user", "pass") == "1q2w3e4r"
+    mock_client.request.assert_called_once_with(
+        "post", "/api/session", headers={"Content-Type": "application/json"},
+        json={"username": "user", "password": "pass"}
+    )
+
