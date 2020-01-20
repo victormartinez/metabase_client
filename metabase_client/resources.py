65022847222e0b0e5ebb999053969dfd313dd85a
Victor Martinez <vcrmartinez@gmail.com>
2020-01-19 22:42:51 -0300

    Add 'execute' method for CardResource class

--- metabase_client/resources.py
+++ metabase_client/resources.py
@@ -19,7 +19,20 @@ class SessionResource(Resource):
 class CardResource(Resource):
 
     PATH_LIST_CARDS = "/api/card"
+    URL_POST_CARD_ID = "/api/card/{}/query"
+    URL_POST_CARD_ID_EXPORT = "/api/card/{}/query/{}"
 
     def list(self, auth_token: str):
         response = self.client.authenticated("get", self.PATH_LIST_CARDS, auth_token)
+        return response.json()
+
+    def execute(self, card_number, data=None, export=None):
+        if export:
+            path = self.URL_POST_CARD_ID_EXPORT.format(card_number, export)
+        else:
+            path = self.URL_POST_CARD_ID.format(card_number)
+
+        response = self.client.authenticated(
+            "post", self.endpoint, path, self.token, json=data
+        )
         return response.json()
